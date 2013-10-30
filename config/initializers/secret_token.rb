deaf0e8966d6c271e9e350cdf3f4b53621558849
rok <leona@simasima.org>
2013-10-30 22:50:43 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pedometer::Application.config.secret_key_base = '1922e39d4a25f443385a3d59c8690bfea22a559448760c12e3c034468be4ce4709ed92f592f9a85c4695fa53c17389bd3473193b9585e25636db9e098b420418'
