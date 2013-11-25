42c1151855e65738dff481f70763b8725b878fdf
rok <leona@simasima.org>
2013-11-26 00:40:21 +0900

    add password reset link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131116094647) do
+ActiveRecord::Schema.define(version: 20131125140229) do
 
   create_table "constants", force: true do |t|
     t.integer  "stride"
@@ -46,8 +46,10 @@ ActiveRecord::Schema.define(version: 20131116094647) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",                  default: false
     t.integer  "team_id"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
